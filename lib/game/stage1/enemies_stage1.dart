ddac1396f03c5097ee8071faf8c2abf07f2399a0
Daniel Lukic <daniel.lukic@gmx.net>
2024-11-21 08:05:26 +0100

    Add wave skipping to EnemiesStage1

--- lib/game/stage1/enemies_stage1.dart
+++ lib/game/stage1/enemies_stage1.dart
@@ -1,15 +1,24 @@
-import 'package:flame/components.dart';
+import 'package:voxone/core/common.dart';
 import 'package:voxone/game/messages.dart';
 import 'package:voxone/game/stage1/enemy_wave.dart';
 import 'package:voxone/game/stage1/marauder_wave.dart';
 import 'package:voxone/game/stage1/minefield_wave.dart';
+import 'package:voxone/util/auto_dispose.dart';
 import 'package:voxone/util/extensions.dart';
 import 'package:voxone/util/messaging.dart';
+import 'package:voxone/util/shortcuts.dart';
 
-class EnemiesStage1 extends Component {
+class EnemiesStage1 extends AutoDisposeComponent with HasAutoDisposeShortcuts {
   final _waves = [MarauderWave(), MinefieldWave()];
 
   EnemyWave? _active_wave;
+
+  @override
+  onLoad() {
+    if (dev) {
+      onKey('<C-w>', () => _active_wave?.removeFromParent());
+    }
+  }
 
   @override
   void update(double dt) {
@@ -19,6 +28,7 @@ class EnemiesStage1 extends Component {
       removeFromParent();
       sendMessage(EnemiesDefeated());
     } else {
+      _active_wave?.removeFromParent();
       _active_wave = added(_waves.removeAt(0));
     }
   }
