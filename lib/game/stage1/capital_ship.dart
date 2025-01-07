3a2bf48220203df487c876fe5e5aad5b6ad97aed
Daniel Lukic <daniel.lukic@gmx.net>
2025-01-07 15:03:51 +0100

    Make CapitalShip have destructible shield generator

--- lib/game/stage1/capital_ship.dart
+++ lib/game/stage1/capital_ship.dart
@@ -1,11 +1,13 @@
 import 'dart:math';
 
+import 'package:dart_minilog/dart_minilog.dart';
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flutter/animation.dart';
 import 'package:voxone/aural/soundboard.dart';
 import 'package:voxone/core/common.dart';
 import 'package:voxone/core/traits.dart';
+import 'package:voxone/game/shared/enemy_explosion.dart';
 import 'package:voxone/game/shared/enemy_health_bar.dart';
 import 'package:voxone/game/shared/shadows.dart';
 import 'package:voxone/game/shared/stacked_entity.dart';
@@ -17,6 +19,7 @@ class CapitalShip extends MarauderEntity
         _CreateCapitalShipEntity,
         _VibrateOnIncoming,
         AddShieldAfterOnIncoming,
+        _LoseShieldWhenGeneratorDestroyed,
         _MaintainSatellitesOnActive,
         NopOnSweeping,
         NopOnLeaving,
@@ -28,7 +31,7 @@ class CapitalShip extends MarauderEntity
   void shield_added() {
     super.shield_added();
     shield.size.setFrom(entity.size);
-    shield.auto_recharge = 0.05;
+    shield.auto_recharge = dev ? 0.01 : 0.05;
     shield.max_rotate_time = 360;
     indicator.scale.setAll(0.25);
     indicator.position.setValues(0, -16);
@@ -36,6 +39,8 @@ class CapitalShip extends MarauderEntity
 }
 
 mixin _CreateCapitalShipEntity on MarauderEntity {
+  late CircleHitbox center_mass;
+
   @override
   createEntity() async {
     reset_hit_points_to(150);
@@ -57,7 +62,35 @@ mixin _CreateCapitalShipEntity on MarauderEntity {
 
     await add(entity);
 
-    await add(CircleHitbox(collisionType: CollisionType.passive, anchor: Anchor.center)
+    await add(center_mass = CircleHitbox.relative(
+      0.25,
+      parentSize: size,
+      position: Vector2(0, -10),
+      collisionType: CollisionType.passive,
+      anchor: Anchor.center,
+    )
+      ..paint.color = red
+      ..opacity = 0.2
+      ..renderShape = debug);
+
+    await add(CircleHitbox.relative(
+      0.25,
+      parentSize: size,
+      position: Vector2(-80, -20),
+      collisionType: CollisionType.passive,
+      anchor: Anchor.center,
+    )
+      ..paint.color = red
+      ..opacity = 0.2
+      ..renderShape = debug);
+
+    await add(CircleHitbox.relative(
+      0.25,
+      parentSize: size,
+      position: Vector2(-30, 50),
+      collisionType: CollisionType.passive,
+      anchor: Anchor.center,
+    )
       ..paint.color = red
       ..opacity = 0.2
       ..renderShape = debug);
@@ -99,7 +132,26 @@ mixin _VibrateOnIncoming on MarauderEntity {
   }
 }
 
-mixin _LoseShieldWhenGeneratorDestroyed {}
+mixin _LoseShieldWhenGeneratorDestroyed on _CreateCapitalShipEntity, AddShieldAfterOnIncoming {
+  final _nop = <Vector2>{};
+  double _generator_hit_points = dev ? 3 : 25;
+
+  @override
+  void on_hit({Set<Vector2>? intersections, double damage = 1}) {
+    final center_hit = (intersections ?? _nop).any(center_mass.containsPoint);
+    if (state == MarauderState.active && center_hit && _generator_hit_points > 0) {
+      _generator_hit_points = max(0, _generator_hit_points - damage);
+      if (_generator_hit_points <= 0) {
+        logInfo('Shield generator destroyed');
+        shield.removeFromParent();
+        indicator.removeFromParent();
+        entity.add(EnemyExplosion()..scale.setAll(0.2));
+      }
+    }
+
+    super.on_hit(intersections: intersections, damage: center_hit ? damage * 2.5 : damage);
+  }
+}
 
 mixin _MaintainSatellitesOnActive on MarauderEntity, HasTraits {
   final _satellites = <MarauderEntity>[];
