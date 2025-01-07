56538569a1751d1a9b845de4545c62f518e6d49d
Daniel Lukic <daniel.lukic@gmx.net>
2025-01-07 06:00:57 +0100

    Make collision detection available via HasContext

--- lib/game/shared/has_context.dart
+++ lib/game/shared/has_context.dart
@@ -1,21 +1,25 @@
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:voxone/game/shared/game_screen.dart';
 import 'package:voxone/game/shared/stage_cache.dart';
 import 'package:voxone/util/keys.dart';
-
-extension GameScreenExtensions on GameScreen {
-  T first<T>() => descendants(includeSelf: true).whereType<T>().first;
-}
 
 /// Mixin to provide cross-component-hierarchy access to other, shared components. The [stage] is always required as
 /// the root. The [cache] is used to lookup everything only once. Many components need shared components like for
 /// example [Decals]. To not have all this references explicitly everywhere, this mixin is used instead.
 mixin HasContext on Component {
   GameScreen? _stage;
+  CollisionDetection<ShapeHitbox, Sweep<ShapeHitbox>>? _collision;
 
   GameScreen get stage => _stage ??= findParent<GameScreen>(includeSelf: true)!;
 
   StageCache get cache => stage.stage_cache;
 
   Keys get keys => stage.stage_keys;
+
+  CollisionDetection<ShapeHitbox, Sweep<ShapeHitbox>> get collisionDetection => _collision ??= stage
+      .ancestors(includeSelf: true)
+      .whereType<HasCollisionDetection<Sweep<ShapeHitbox>>>()
+      .first
+      .collisionDetection;
 }
