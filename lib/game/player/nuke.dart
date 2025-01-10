bce9e3e4d4f28982cea0d41ff07d59b44472aee4
Daniel Lukic <daniel.lukic@gmx.net>
2025-01-10 07:24:41 +0100

    Decrease nuke radius

--- lib/game/player/nuke.dart
+++ lib/game/player/nuke.dart
@@ -12,7 +12,7 @@ import 'package:voxone/util/extensions.dart';
 
 class Nuke extends PositionComponent with CollisionCallbacks, Recyclable, DirectionalProjectile, HasPaint {
   Nuke() {
-    size.setAll(400);
+    size.setAll(300);
     add(CircleHitbox(anchor: Anchor.center, isSolid: true)
       ..renderShape = debug
       ..paint.opacity = 0.2);
