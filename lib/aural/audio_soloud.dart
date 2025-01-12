7a3b1164c78b316a757322cfe9b89df02a97d0fb
Daniel Lukic <daniel.lukic@gmx.net>
2025-01-12 15:05:05 +0100

    Remove log output from AudioSystem

--- lib/aural/audio_soloud.dart
+++ lib/aural/audio_soloud.dart
@@ -114,7 +114,6 @@ class PlatformAudioSystem extends AudioSystem {
 
     final source = _one_shots.putIfAbsent(filename, () => soloud.loadAsset('assets/audio/$filename'));
     final volume = (volume_factor * super.sound * super.master).clamp(0.0, volume_factor * master);
-    logInfo('play one shot sample: $volume_factor $sound $master => $volume');
     final active = await source;
     final handle = await soloud.play(active, volume: volume, looping: loop);
 
--- lib/aural/audio_system.dart
+++ lib/aural/audio_system.dart
@@ -182,7 +182,6 @@ abstract class AudioSystem extends Component {
     bool cache = true,
     bool loop = false,
   }) async {
-    logInfo('play one shot sample $filename volume=$volume_factor');
     if (_muted) return Disposable.disposed;
     return await do_play_one_shot_sample(filename, volume_factor: volume_factor, cache: cache, loop: loop);
   }
