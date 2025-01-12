782f37faa5758edb7736c144109e3f31e096a05d
Daniel Lukic <daniel.lukic@gmx.net>
2025-01-12 09:41:30 +0100

    Make MarauderShot stronger

--- lib/game/stage1/marauder_shot.dart
+++ lib/game/stage1/marauder_shot.dart
@@ -55,7 +55,7 @@ class MarauderShot extends PositionComponent with CollisionCallbacks, HasContext
     if (other.hasTrait<Friendly>()) {
       other.onTraits<Target>((it) {
         if (!it.susceptible) return;
-        it.on_hit(damage: 1);
+        it.on_hit(damage: 2.5);
         recycle();
       });
     }
