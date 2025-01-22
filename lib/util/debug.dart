6e065bb23170c203e3a18e0602a9a9b7cb9206b8
Daniel Lukic <daniel.lukic@gmx.net>
2025-01-22 06:20:02 +0100

    Remove obsolete DebugCircleHitbox

--- lib/util/debug.dart
+++ lib/util/debug.dart
@@ -5,28 +5,6 @@ import 'package:voxone/core/common.dart';
 
 Color? debugHitboxColor = const Color(0x80ff0000);
 
-class DebugCircleHitbox extends CircleComponent with HasVisibility {
-  DebugCircleHitbox({
-    super.radius,
-    super.position,
-    super.scale,
-    super.angle,
-    super.anchor = Anchor.center,
-    super.children,
-    super.priority,
-    super.paint,
-    super.paintLayers,
-  }) {
-    paint.style = PaintingStyle.stroke;
-    paint.strokeWidth = 1;
-    if (debugHitboxColor != null) paint.color = debugHitboxColor!;
-    priority = 20;
-  }
-
-  @override
-  bool get isVisible => debug;
-}
-
 class DebugText extends TextComponent with HasVisibility {
   DebugText({required String Function() text, super.anchor, super.position, double? scale}) {
     _text = text;
