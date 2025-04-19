8550ba70aa36979148a42fd47ad93693d8674409
Daniel Lukic <daniel.lukic@gmx.net>
2025-04-19 20:13:03 +0200

    tmp

--- lib/game/manta.dart
+++ lib/game/manta.dart
@@ -94,13 +94,13 @@ class MantaComponent extends PositionComponent with HasContext, HasPaint {
   Future<void> onLoad() async {
     logInfo('Loading Manta');
     try {
-      _voxelImage = await game.images.load('ZaxxonPlayer-15.png');
+      _voxelImage = await game.images.load('manta_19.png');
     } catch (e) {
       logError('Error loading voxel image: $e');
       return;
     }
     assert(_voxelImage != null, 'Voxel image failed to load.');
-    _frames = 15;
+    _frames = 19;
     try {
       _shader = await loadShader('voxel3d.frag');
       // Initialize Uniforms with the FULL enum
