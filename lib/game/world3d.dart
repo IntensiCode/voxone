fb5174a212cf7878d1a9dbc9addf1164746329b0
Daniel Lukic <daniel.lukic@gmx.net>
2025-04-20 08:19:51 +0200

    tmp

--- lib/game/world3d.dart
+++ lib/game/world3d.dart
@@ -30,13 +30,11 @@ class World3d {
       // 1. Build Scale Matrix (S)
       _scaleMatrix.scale(child.scale3D);
 
-      // 2. Build Rotation Matrix (R)
-      final rotX = Matrix4.rotationX(child.rotation.x);
-      final rotY = Matrix4.rotationY(child.rotation.y);
-      final rotZ = Matrix4.rotationZ(child.rotation.z);
-      _rotationMatrix.multiply(rotZ);
-      _rotationMatrix.multiply(rotY);
-      _rotationMatrix.multiply(rotX);
+      // 2. Build Rotation Matrix (R) directly on _rotationMatrix
+      // Apply rotations in Z, Y, X order (same effective order as previous Z*Y*X multiplication)
+      _rotationMatrix.rotateZ(child.rotation.z);
+      _rotationMatrix.rotateY(child.rotation.y);
+      _rotationMatrix.rotateX(child.rotation.x);
 
       // 3. Build Translation Matrix (T)
       _translationMatrix.translate(child.position3D);
@@ -46,29 +44,42 @@ class World3d {
       _worldModelMatrix.multiply(_rotationMatrix);
       _worldModelMatrix.multiply(_scaleMatrix);
 
-      child.projectedVertices.clear();
-      double totalNdcZ = 0;
-      int visibleVertexCount = 0;
+      _projectChild(child);
+    }
+  }
 
-      for (final localVertex in child.localVertices) {
-        final worldVertex = _worldModelMatrix.transform3(localVertex);
-        final Vector3? ndc = camera.projectWorldToNdc(worldVertex);
-        Vector2? screenPos;
-        if (ndc != null) {
-          screenPos = camera.ndcToScreen(ndc, _screenSize);
-          totalNdcZ += ndc.z;
-          visibleVertexCount++;
-        }
-        child.projectedVertices.add(screenPos);
-      }
+  final Vector3 _project = Vector3.zero();
+  
+  void _projectChild(PositionComponent3D child) {
+    // Ensure child.projectedVertices has same size as child.localVertices:
+    while (child.projectedVertices.length < child.localVertices.length) {
+      child.projectedVertices.add(Vector2.zero());
+    }
+    while (child.projectedVertices.length > child.localVertices.length) {
+      child.projectedVertices.removeLast();
+    }
 
-      // Priority calculation
-      if (visibleVertexCount > 0) {
-        final averageNdcZ = totalNdcZ / visibleVertexCount;
-        child.priority = Camera3D.depthToPriority(averageNdcZ);
-      } else {
-        child.priority = -1;
+    double totalNdcZ = 0;
+    int visibleVertexCount = 0;
+
+    for (final localVertex in child.localVertices) {
+      final worldVertex = _worldModelMatrix.transform3(localVertex);
+      final Vector3? ndc = camera.projectWorldToNdc(worldVertex);
+      Vector2? screenPos;
+      if (ndc != null) {
+        screenPos = camera.ndcToScreen(ndc);
+        totalNdcZ += ndc.z;
+        visibleVertexCount++;
       }
+      child.projectedVertices.add(screenPos);
+    }
+
+    // Priority calculation
+    if (visibleVertexCount > 0) {
+      final averageNdcZ = totalNdcZ / visibleVertexCount;
+      child.priority = Camera3D.depthToPriority(averageNdcZ);
+    } else {
+      child.priority = -1;
     }
   }
 }
